6bb3949877b15c084c094b1c9f32eecd8f2bd7ad
ph3l <pultra@outlook.com>
2021-10-05 21:29:44 +1000

    Need fix the date part

--- App5/App5/MainPage.xaml.cs
+++ App5/App5/MainPage.xaml.cs
@@ -9,6 +9,8 @@ using HtmlAgilityPack;
 using System.Text.RegularExpressions;
 using System.Net.Http;
 
+//adb connect 127.0.0.1:5555
+
 /*
 iterate l
 */
@@ -49,7 +51,7 @@ namespace App5
 
                 await App.Database.SaveTideAsync(new TideInformation
                 {
-                    Date = "4/10/2021", 
+                    // Date = *******Fix this part***********, 
                     // check if the date already exist
 
                     firstLowTide = myResult.Item1,
@@ -61,10 +63,11 @@ namespace App5
             }
         }
 
+
         public static Tuple<string, string, string, string> getTideData(string todaysDate) // make this so you can set the day here
         {
-            // get the current date and its associated tide times []
-            // get the data for the next 5 days and store in sqlite []
+            // get the current date and its associated tide times [x]
+            // get the data for the next 5 days and store in sqlite [x]
             // Todays date
             // 1 = today, 2 = tomorrow
             string firstLowTide, firstHighTide, secondLowTide, secondHighTide;
@@ -74,17 +77,23 @@ namespace App5
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load(url);
 
-            //5:16 am1.1m10:53 am0.35m5:36 pm1.64m -> string result
-            //\d+\W\\d+\d+
+            // 5:16 am1.1m10:53 am0.35m5:36 pm1.64m -> regex pattern -> \d+\W\\d+\d+
+            // "Today 5 Oct1:25 am0.09m7:35 am1.51m1:19 pm0.07m7:48 pm1.83m" -> need to parse out the date then convert to datetime use the code below to do this
+            /*
+            	var mydate = DateTime.ParseExact("5 Jan", "d MMM", CultureInfo.InvariantCulture);
+	            Console.WriteLine(mydate);
+
+                use another regex pattern to get the date from the string, then append this to sqlite
+            */
+
             List<string> tideResults = new List<string>();
-            //var items = htmlDoc.DocumentNode.SelectNodes("//div/ul/li[1]/ul");
-            var items = htmlDoc.DocumentNode.SelectNodes($"//div/ul/li[{todaysDate}]/ul"); //
+            var items = htmlDoc.DocumentNode.SelectNodes($"//div/ul/li[{todaysDate}]"); // $"//div/ul/li[{todaysDate}]/ul")
             foreach (var item in items)
             {
                 tideResults.Add(item.InnerText);
             }
             // Part 2: call Regex.Match.
-            string str = tideResults[0];
+            string str = tideResults[2];
             string[] result =
               Regex.Matches(str, @"((1[0-2]|0?[1-9]):([0-5][0-9]) \s?([AaPp][Mm]))").Cast<Match>().Select(m => m.Value).ToArray();
 
